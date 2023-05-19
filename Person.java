5e3841a41195fc00dcb6fd3d85d6449e79224221
jordanhanleybrown <133904671+jordanhanleybrown@users.noreply.github.com>
2023-05-19 12:43:22 -0700

    Player update

--- Person.java
+++ Person.java
@@ -2,12 +2,73 @@ import java.util.*;
 public class Person { 
     private String name; 
     private double money; 
-    private double gas; 
-    private double mileage;
-    private String car; 
+    private Car auto;
+    private int hunger;
 
-    public Person(String name){ 
-        
+    public Person(String name, Car auto){  
+        this.name = name;  
+        this.auto = auto;   
+        hunger = 100;
+        money = 1000;
+    } 
+
+    public boolean setHunger(int a)
+    { 
+        //returns false if starved, true if okay 
+        if((hunger+a)<100){ 
+            if((hunger+a)<0){ 
+                hunger = 0; 
+                return false; 
+            } 
+            else{ 
+                hunger+=a; 
+                return true;
+            } 
+        } 
+        else{ 
+            hunger = 100; 
+            return true;
+        }
+            }  
+
+    public int getHunger(){ 
+        return hunger;
     }
 
-}
+    public boolean setMoney(int a)
+    {
+        //returns false if broke, true if okay 
+         
+        if((money+a)<0){ 
+                money = 0; 
+                return false; 
+            } 
+            else{ 
+                money+=a; 
+                return true;
+            } 
+        }     
+
+    public double getMoney(){ 
+        return money;
+    } 
+
+    public Car getCar(){ 
+        return auto;
+    } 
+
+    public String getName(){ 
+        return name; 
+    }
+    }
+
+       
+    
+            
+        
+            
+
+
+    
+
+
