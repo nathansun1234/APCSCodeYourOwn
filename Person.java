3506214dac0fd31e58c66fdc5618ac0688596382
jordanhanleybrown <133904671+jordanhanleybrown@users.noreply.github.com>
2023-05-19 12:08:30 -0700

    person

---
+++ Person.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+public class Person { 
+    private String name; 
+    private double money; 
+    private double gas; 
+    private double mileage;
+    private String car; 
+
+    public Person(String name){ 
+        
+    }
+
+}
